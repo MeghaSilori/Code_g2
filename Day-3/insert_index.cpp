b97fb47bda18d968c8709365e87724e49221be55
MeghaSilori <meghasilori7@gmail.com>
2021-07-02 19:49:40 +0530

    day3

--- Day-3/insert_index.cpp
+++ Day-3/insert_index.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
-        vector<int> target;
+        vector<int> target(nums.size());
         for(int i=0;i<nums.size();i++){
-            target.insert(target.begin()+index[i],nums[i]);
+            for(int j=nums.size()-1; j>index[i]; j--){
+                target[j]=target[j-1];
+            }
+            target[index[i]] = nums[i];
         }
         return target;
     }
